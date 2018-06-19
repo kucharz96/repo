2f1acae0da6a18d00c24b003c868de51a09802d9
kucharz96 <kucharz96@wp.pl>
2018-06-19 02:22:34 +0200

    s

--- projekt/src/application/Main.java
+++ projekt/src/application/Main.java
@@ -1,26 +1,63 @@
 package application;
-	
+import javafx.collections.ObservableList; 
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
-
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.geometry.Insets; 
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        Button btn2 = new Button();
+        Button btn3 = new Button();
+        Label label1 = new Label("Przechodnia");
+        Pane root = new Pane();
+        label1.setFont(new Font("Arial", 38));
+        ObservableList list = root.getChildren();
+        btn.setText("Pacjenci");
+        btn2.setText("Lekarze");
+        
+        btn3.setText("Exit");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+         list.clear();
+
+            }
+        });
+        
+        
+        label1.setLayoutX(190);
+        label1.setLayoutY(40);
+        btn.setLayoutX(150);
+        btn.setLayoutY(120);
+        btn2.setLayoutX(150);
+        btn2.setLayoutY(220);
+        btn3.setLayoutX(430);
+        btn3.setLayoutY(350);
+        btn.setMinWidth(300);
+        btn.setMinHeight(75);
+        btn2.setMinWidth(300);
+        btn2.setMinHeight(75);
+        btn3.setMinWidth(100);
+        
+        list.addAll(btn,btn2,btn3,label1);
+        Scene scene = new Scene(root, 600, 400);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+ public static void main(String[] args) {
+        launch(args);
+    }
 }
